74dcadc260387e15d2cf676d17bde849e7baac4c
Boris Laskov <b.v.laskov@gmail.com>
2018-02-01 23:53:33 +0100

    Make API calls return body or raise errors

--- classification/classification.py
+++ classification/classification.py
@@ -1,6 +1,7 @@
 from classification.sessionutils import get_session_from_token, \
     SavedTokenError, get_new_session, save_token
-from classification.utils import get_body_or_empty_dict
+from classification.utils import get_body_or_empty_dict, \
+    get_body_or_raise_error
 from oauthlib.oauth2 import TokenExpiredError
 from requests.auth import HTTPBasicAuth
 
@@ -72,49 +73,68 @@ class Classification:
     @refresh_token
     def delete_classification(self, course_code, classification_id,
                               semester=None, **kwargs):
+
         params = {'classification-identifier': classification_id,
                   'semester': semester}
-        return self.session.delete(f'{self.API_URL}/public'
+
+        resp = self.session.delete(f'{self.API_URL}/public'
                                    f'/courses/{course_code}'
                                    f'/classifications',
                                    params=params, **kwargs)
 
+        return get_body_or_raise_error(resp, 204)
+
     @refresh_token
     def find_classifications_for_course(self, course_code, semester=None,
                                         lang=None, **kwargs):
+
         params = {'semester': semester, 'lang': lang}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/courses/{course_code}'
                                 f'/classifications',
                                 params=params, **kwargs)
+
+        return get_body_or_raise_error(resp, 200)
 
     @refresh_token
     def save_classification(self, course_code, classification_dto=None,
                             **kwargs):
         body = get_body_or_empty_dict(classification_dto)
-        return self.session.post(f'{self.API_URL}/public'
+
+        resp = self.session.post(f'{self.API_URL}/public'
                                  f'/courses/{course_code}'
                                  f'/classifications',
                                  json=body, **kwargs)
 
+        return get_body_or_raise_error(resp, 201)
+
     @refresh_token
     def change_order_of_classifications(self, course_code, indexes,
                                         semester=None, **kwargs):
+
         params = {'semester': semester}
-        return self.session.put(f'{self.API_URL}/public'
+
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/courses/{course_code}'
                                 f'/classifications/order',
                                 params=params, json=indexes,
                                 **kwargs)
 
+        return get_body_or_raise_error(resp, 201)
+
     @refresh_token
     def find_classification(self, course_code, identifier, semester=None,
                             lang=None, **kwargs):
+
         params = {'semester': semester, 'lang': lang}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/courses/{course_code}'
                                 f'/classifications/{identifier}',
                                 params=params, **kwargs)
+
+        return get_body_or_raise_error(resp, 200)
 
     @refresh_token
     def clone_classification_definitions(self, target_semester,
@@ -123,37 +143,50 @@ class Classification:
                                          source_course_code,
                                          remove_existing,
                                          **kwargs):
+
         params = {'target-semester': target_semester,
                   'source-semester': source_semester,
                   'remove-existing': remove_existing}
-        return self.session.put(f'{self.API_URL}/public'
+
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/courses/{source_course_code}'
                                 f'/classifications'
                                 f'/clones/{target_course_code}',
                                 params=params, **kwargs)
+
+        return get_body_or_raise_error(resp, 201)
 
     # -----------------------------------------------
     # -------------- EDITOR CONTROLLER --------------
     # -----------------------------------------------
     @refresh_token
     def get_editors(self, course_code, **kwargs):
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/courses/{course_code}/editors',
                                 **kwargs)
 
+        return get_body_or_raise_error(resp, 200)
+
     @refresh_token
     def delete_editor(self, course_code, username, **kwargs):
-        return self.session.delete(f'{self.API_URL}/public'
+
+        resp = self.session.delete(f'{self.API_URL}/public'
                                    f'/courses/{course_code}'
                                    f'/editors/{username}',
                                    **kwargs)
 
+        return get_body_or_raise_error(resp, 204)
+
     @refresh_token
     def add_editor(self, course_code, username, **kwargs):
-        return self.session.put(f'{self.API_URL}/public'
+
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/courses/{course_code}'
                                 f'/editors/{username}',
                                 **kwargs)
+
+        return get_body_or_raise_error(resp, 201)
 
     # -----------------------------------------------
     # ------------ EXPRESSION CONTROLLER ------------
@@ -161,22 +194,31 @@ class Classification:
     @refresh_token
     def evaluate_all(self, expressions_dto=None, **kwargs):
         body = get_body_or_empty_dict(expressions_dto)
-        return self.session.post(f'{self.API_URL}/public'
+
+        resp = self.session.post(f'{self.API_URL}/public'
                                  f'/course-expressions/analyses',
                                  json=body, **kwargs)
+
+        return get_body_or_raise_error(resp, 201)
 
     @refresh_token
     def try_validity(self, expression=None, **kwargs):
         body = get_body_or_empty_dict(expression)
-        return self.session.post(f'{self.API_URL}/public'
+
+        resp = self.session.post(f'{self.API_URL}/public'
                                  f'/expressions/analyses',
                                  json=body, **kwargs)
 
+        return get_body_or_raise_error(resp, 201)
+
     @refresh_token
     def get_functions(self, **kwargs):
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/expressions/functions',
                                 **kwargs)
+
+        return get_body_or_raise_error(resp, 200)
 
     # -----------------------------------------------
     # ----------- NOTIFICATION CONTROLLER -----------
@@ -184,76 +226,111 @@ class Classification:
     @refresh_token
     def get_all_notifications(self, username, count=None, page=None,
                               lang=None, **kwargs):
+
         params = {'count': count, 'page': page, 'lang': lang}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/notifications/{username}/all',
                                 params=params, **kwargs)
+
+        return get_body_or_raise_error(resp, 200)
 
     @refresh_token
     def get_unread_notifications(self, username, count=None, page=None,
                                  lang=None, **kwargs):
+
         params = {'count': count, 'page': page, 'lang': lang}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/notifications/{username}/new',
                                 params=params, **kwargs)
 
+        return get_body_or_raise_error(resp, 200)
+
     @refresh_token
     def unread_all_notifications(self, username, **kwargs):
-        return self.session.delete(f'{self.API_URL}/public'
+
+        resp = self.session.delete(f'{self.API_URL}/public'
                                    f'/notifications/{username}/read',
                                    **kwargs)
 
+        return get_body_or_raise_error(resp, 204)
+
     @refresh_token
     def read_all_notifications(self, username, **kwargs):
-        return self.session.put(f'{self.API_URL}/public'
+
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/notifications/{username}/read',
                                 **kwargs)
 
+        return get_body_or_raise_error(resp, 201)
+
     @refresh_token
     def unread_notification(self, username, id, **kwargs):
-        return self.session.delete(f'{self.API_URL}/public'
+
+        resp = self.session.delete(f'{self.API_URL}/public'
                                    f'/notifications/{username}/read/{id}',
                                    **kwargs)
 
+        return get_body_or_raise_error(resp, 204)
+
     @refresh_token
     def read_notification(self, username, id, **kwargs):
-        return self.session.put(f'{self.API_URL}/public'
+
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/notifications/{username}/read/{id}',
                                 **kwargs)
+
+        return get_body_or_raise_error(resp, 201)
 
     # -----------------------------------------------
     # ------------- SETTINGS CONTROLLER -------------
     # -----------------------------------------------
     @refresh_token
     def get_settings(self, semester=None, lang=None, **kwargs):
+
         params = {'semester': semester, 'lang': lang}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/settings/my',
                                 params=params, **kwargs)
+
+        return get_body_or_raise_error(resp, 200)
 
     @refresh_token
     def save_student_course_settings(self, user_settings_dto=None, **kwargs):
         body = get_body_or_empty_dict(user_settings_dto)
-        return self.session.put(f'{self.API_URL}/public'
+
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/settings/my', json=body, **kwargs)
+
+        return get_body_or_raise_error(resp, 201)
 
     @refresh_token
     def save_student_course_settings(self, user_course_settings_dto=None,
                                      semester=None, **kwargs):
+
         params = {'semester': semester}
         body = get_body_or_empty_dict(user_course_settings_dto)
-        return self.session.put(f'{self.API_URL}/public'
+
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/settings/my/student/courses',
                                 params=params, json=body, **kwargs)
+
+        return get_body_or_raise_error(resp, 201)
 
     @refresh_token
     def save_teacher_course_settings(self, user_course_settings_dto=None,
                                      semester=None, **kwargs):
+
         params = {'semester': semester}
         body = get_body_or_empty_dict(user_course_settings_dto)
-        return self.session.put(f'{self.API_URL}/public'
+
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/settings/my/teacher/courses',
                                 params=params, json=body, **kwargs)
+
+        return get_body_or_raise_error(resp, 201)
 
     # -----------------------------------------------
     # ------ STUDENT CLASSIFICATION CONTROLLER ------
@@ -261,28 +338,37 @@ class Classification:
     @refresh_token
     def find_student_group_classifications(self, course_code, group_code,
                                            semester=None, **kwargs):
+
         params = {'semester': semester}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/courses/{course_code}'
                                 f'/group/{group_code}'
                                 f'/student-classifications',
                                 params=params, **kwargs)
 
+        return get_body_or_raise_error(resp, 200)
+
     @refresh_token
     def find_student_classifications_for_definitions(self, course_code,
                                                      identifier, group_code,
                                                      semester=None, **kwargs):
+
         params = {'semester': semester}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/courses/{course_code}'
                                 f'/group/{group_code}'
                                 f'/student-classifications/{identifier}',
                                 params=params, **kwargs)
 
+        return get_body_or_raise_error(resp, 200)
+
     @refresh_token
     def save_student_classifications(self, course_code,
                                      student_classifications=None,
                                      semester=None, **kwargs):
+
         params = {'semester': semester}
 
         if student_classifications is not None:
@@ -290,20 +376,26 @@ class Classification:
         else:
             body = dict()
 
-        return self.session.put(f'{self.API_URL}/public'
+        resp = self.session.put(f'{self.API_URL}/public'
                                 f'/courses/{course_code}'
                                 f'/student-classifications',
                                 params=params, json=body, **kwargs)
 
+        return get_body_or_raise_error(resp, 201)
+
     @refresh_token
     def find_student_classification(self, course_code, student_username,
                                     semester=None, lang=None, **kwargs):
+
         params = {'semester': semester, 'lang': lang}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/courses/{course_code}'
                                 f'/student-classifications'
                                 f'/{student_username}',
                                 params=params, **kwargs)
+
+        return get_body_or_raise_error(resp, 200)
 
     # -----------------------------------------------
     # ---------- STUDENT GROUP CONTROLLER -----------
@@ -311,8 +403,12 @@ class Classification:
     @refresh_token
     def get_course_groups(self, course_code,
                           semester=None, lang=None, **kwargs):
+
         params = {'semester': semester, 'lang': lang}
-        return self.session.get(f'{self.API_URL}/public'
+
+        resp = self.session.get(f'{self.API_URL}/public'
                                 f'/course/{course_code}'
                                 f'/student-groups',
                                 params=params, **kwargs)
+
+        return get_body_or_raise_error(resp, 200)
--- classification/utils.py
+++ classification/utils.py
@@ -6,3 +6,23 @@ def remove_none_entries(dictionary):
 
 def get_body_or_empty_dict(object):
     return object.body if object is not None else dict()
+
+
+def get_body_or_raise_error(resp, exp_code):
+    if resp.status_code == exp_code and exp_code == 204:
+        return None  # Since 204 is for 'No Content'
+
+    if resp.status_code == exp_code:
+        try:
+            body = resp.json()
+            if len(body) == 0:
+                return None
+            else:
+                return body
+
+        except ValueError:
+            # For the requests with 'optional' body
+            # that can return 201, for example
+            return None
+
+    resp.raise_for_status()
