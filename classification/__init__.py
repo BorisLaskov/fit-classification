75c5e20dca35346f6534dc847e879b3b8f9fc0a3
Boris Laskov <b.v.laskov@gmail.com>
2018-02-07 22:12:22 +0100

    Add setup configuration

--- classification/__init__.py
+++ classification/__init__.py
@@ -1,3 +1,21 @@
 from .classification import Classification
+from .classificationproxy import ClassificationParamsProxy
+from .exceptions import AuthError, SavedTokenError, \
+    MissingParameterError
+from .entities import ClassificationTextDto, ClassificationDto,\
+    StudentClassificationPreviewDto, UserSettingsDto, \
+    UserCourseSettingsDto, ExpressionParseAllRequestDto, \
+    ExpressionParseRequestDto
 
-__all__ = ['Classification']
+__all__ = ['Classification',
+           'ClassificationParamsProxy',
+           'AuthError',
+           'SavedTokenError',
+           'MissingParameterError',
+           'ClassificationTextDto',
+           'ClassificationDto',
+           'StudentClassificationPreviewDto',
+           'UserSettingsDto',
+           'UserCourseSettingsDto',
+           'ExpressionParseAllRequestDto',
+           'ExpressionParseRequestDto']
--- setup.py
+++ setup.py
@@ -1,27 +1,34 @@
 from setuptools import setup
 
 
-# with open('README.rst') as f:
-#     long_description = ''.join(f.readlines())
+with open('README.rst') as f:
+    long_description = ''.join(f.readlines())
 
 setup(
     name='fit_classification',
     version='0.1',
-    description='todo',
-    long_description='todo',
+    description='Access Classification portal API from your Python programs',
+    long_description=long_description,
     author='Boris Laskov',
     author_email='laskobor@fit.cvut.cz',
-    keywords='todo',
-    license='todo',
-    url='todo',
+    keywords='CTU FIT,Classification,coursework',
+    license='MIT License',
+    url='https://github.com/145k0v/classification-python',
     packages=['classification'],
-    # package_data={...},
     zip_safe=False,
     install_requires=['requests>=2.18.4', 'requests-oauthlib>=0.8.0',
                       'appdirs>=1.4.3', 'dataclasses>=0.4'],
     setup_requires=['pytest-runner>=3.0'],
     tests_require=['pytest>=3.4.0', 'flexmock>=0.10.2', 'betamax>=0.8.0'],
-    # classifiers=[
-    #     ...
-    #     ]
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Environment :: Console',
+        ]
 )
