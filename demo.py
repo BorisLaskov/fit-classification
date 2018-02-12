054816eafdf160342a06b1866a1f857c17d95da3
Boris Laskov <b.v.laskov@gmail.com>
2018-02-12 14:50:02 +0100

    Update demo (remove .json())

--- demo.py
+++ demo.py
@@ -7,4 +7,4 @@ client_secret = 'secret'
 
 c = Classification(client_id, client_secret)
 
-pprint(c.find_student_classification('MI-PYT', 'laskobor').json())
+pprint(c.find_student_classification('MI-PYT', 'laskobor'))
